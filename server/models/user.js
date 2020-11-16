f5464167769646bff8ff3f2f81cbe8add5ef8197
Kristy Li <kristyli20.0@gmail.com>
2020-11-15 22:41:10 -0500

    fix: Change passwordHash to password to delete the password of returnedObject

--- server/models/user.js
+++ server/models/user.js
@@ -25,7 +25,7 @@ UserSchema.set('toJSON', {
 
     delete returnedObject._id;
     delete returnedObject.__v;
-    delete returnedObject.passwordHash;
+    delete returnedObject.password;
   },
 });
 
