6598a763cf0c274b274f0e756faf85dba7f5de61
Kristy Li <kristyli20.0@gmail.com>
2020-11-15 20:23:30 -0500

    feat: Create User model

---
+++ server/models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    require: true,
+  },
+  lastName: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+
+UserSchema.set('toJSON', {
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash;
+  },
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
