34defc2b30783947954c666a054e7fe2fa0413f0
Kristy Li <kristyli20.0@gmail.com>
2020-11-16 17:11:28 -0500

    fix: Specify the PORT to be 8000 if the environment variable is not defined

--- server/utils/config.js
+++ server/utils/config.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 8000,
   MONGODB_URI: process.env.MONGODB_URI,
   JWT_SECRET: process.env.JWT_SECRET,
 };
