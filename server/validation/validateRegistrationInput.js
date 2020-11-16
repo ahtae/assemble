0cc1eb5ff2fbe5edca752228866c2eef03fe9e29
Kristy Li <kristyli20.0@gmail.com>
2020-11-16 17:12:32 -0500

    fix: Add validation for firstName and lastName fields

--- server/validation/validateRegistrationInput.js
+++ server/validation/validateRegistrationInput.js
@@ -4,6 +4,8 @@ const validText = require('./validText');
 const validateRegistrationInput = (data) => {
   const errors = {};
 
+  data.firstName = validText(data.firstName) ? data.firstName : '';
+  data.lastName = validText(data.lastName) ? data.lastName : '';
   data.email = validText(data.email) ? data.email : '';
   data.password = validText(data.password) ? data.password : '';
 
@@ -21,6 +23,14 @@ const validateRegistrationInput = (data) => {
 
   if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
     errors.password = 'Password must be at least 6 characters!';
+  }
+
+  if (Validator.isEmpty(data.firstName)) {
+    errors.firstName = 'First name field is required!';
+  }
+
+  if (Validator.isEmpty(data.lastName)) {
+    errors.lastName = 'Last name field is required!';
   }
 
   return {
