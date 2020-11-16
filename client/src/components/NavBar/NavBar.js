132d769fab2252aabb7bbe9f8ce6a5e8832a2611
Kristy Li <kristyli20.0@gmail.com>
2020-11-16 07:37:19 -0500

    feat: Create NavBar component

---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  Nav,
+  NavItem,
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
+  return (
+    <div style={{ textAlign: 'start' }}>
+      <Navbar color="faded" light>
+        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav navbar>
+            <NavItem>
+              <Link to="/login" style={{ color: 'black' }}>
+                Login
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/register" style={{ color: 'black' }}>
+                Register
+              </Link>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavBar;
