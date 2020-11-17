f0314f79d2c649c2c71b26aab370d65eced1d4d9
Kristy Li <kristyli20.0@gmail.com>
2020-11-16 19:31:19 -0500

    refactor: Events component

--- client/src/components/Events/Events.js
+++ client/src/components/Events/Events.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import moment from 'moment';
 import { Button, ButtonGroup } from 'reactstrap';
 import './Events.css';
+import Event from './Event/Event';
 
 const Events = ({ history }) => {
   const [events, setEvents] = useState([]);
@@ -25,67 +25,59 @@ const Events = ({ history }) => {
   }, []);
 
   const handleFilterClick = (eventType) => {
-    setFilter(eventType)
+    setFilter(eventType);
   };
 
-  console.log(events);
   return (
     <div>
-      <h2 id="events-title">Events</h2>
-      <div id="filter-buttons">
-        <ButtonGroup>
-          <Button
-            onClick={() => handleFilterClick(null)}
-            active={filter === null}
-          >
-            All
-          </Button>
-          <Button
-            onClick={() => handleFilterClick('dancing')}
-            active={filter === 'dancing'}
-          >
-            Dancing
-          </Button>
-          <Button
-            onClick={() => handleFilterClick('running')}
-            active={filter === 'running'}
-          >
-            Running
-          </Button>
-          <Button
-            onClick={() => handleFilterClick('learning')}
-            active={filter === 'learning'}
-          >
-           Learning
-          </Button>
-          <Button
-            onClick={() => handleFilterClick('art')}
-            active={filter === 'art'}
-          >
-            Art
-          </Button>
-          <Button
-            onClick={() => handleFilterClick('myevents')}
-            active={filter === 'myevents'}
-          >
-            My events
-          </Button>
-        </ButtonGroup>
+      <div>
+        <div id="filter-buttons">
+          <ButtonGroup>
+            <Button
+              onClick={() => handleFilterClick(null)}
+              active={filter === null}
+            >
+              All Events
+            </Button>
+            <Button
+              onClick={() => handleFilterClick('dancing')}
+              active={filter === 'dancing'}
+            >
+              Dancing Events
+            </Button>
+            <Button
+              onClick={() => handleFilterClick('running')}
+              active={filter === 'running'}
+            >
+              Running Events
+            </Button>
+            <Button
+              onClick={() => handleFilterClick('learning')}
+              active={filter === 'learning'}
+            >
+              Learning Events
+            </Button>
+            <Button
+              onClick={() => handleFilterClick('art')}
+              active={filter === 'art'}
+            >
+              Art Events
+            </Button>
+            <Button
+              onClick={() => handleFilterClick('myevents')}
+              active={filter === 'myevents'}
+            >
+              My events
+            </Button>
+          </ButtonGroup>
+        </div>
       </div>
       <div id="events">
         {events.map((event) => {
           return (
-            <div className="event" key={event.id}>
-              <header
-                style={{ backgroundImage: `url(${event.thumbnail_url})` }}
-              >
-                <h3>{event.title}</h3>
-                <p>{moment(event.date).format('L')}</p>
-                <p>Event Price: {parseFloat(event.price)}</p>
-                <p>Event Description: {event.description}</p>
-                <Button color="danger">subscribe</Button>
-              </header>
-            </div>
+            <React.Fragment key={event.id}>
+              <Event event={event} />
+            </React.Fragment>
           );
         })}
       </div>
