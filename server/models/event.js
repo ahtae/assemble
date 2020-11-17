c56af905017c958c9f2ef6d835cdae25dcec03a6
Kristy Li <kristyli20.0@gmail.com>
2020-11-16 19:20:33 -0500

    fix: Event model so that the thumbnail url is returned properly

--- server/models/event.js
+++ server/models/event.js
@@ -1,37 +1,34 @@
 const mongoose = require('mongoose');
 
-const EventSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    thumbnail: {
-      type: String,
-    },
-    date: {
-      type: Date,
-      default: Date.now
-    },
-    eventType: {
-      type: String,
-      required: true,
-    },
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-    },
+const EventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
   },
-  {
-    toJSON: {
-      virtuals: true,
-    },
-  }
-);
+  description: {
+    type: String,
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  eventType: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  price: {
+    type: Number,
+    default: 0.0,
+  },
+});
 
 EventSchema.set('toJSON', {
   transform: (_document, returnedObject) => {
@@ -40,6 +37,7 @@ EventSchema.set('toJSON', {
     delete returnedObject._id;
     delete returnedObject.__v;
   },
+  virtuals: true,
 });
 
 EventSchema.virtual('thumbnail_url').get(function () {
