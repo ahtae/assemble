db40508dd9cd4306ff62c8d7f41ccc59c9745e8b
Kristy Li <kristyli20.0@gmail.com>
2020-11-16 19:19:20 -0500

    fix: Upload configuration file in order to save uploaded images

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -1,14 +1,16 @@
 const multer = require('multer');
 const path = require('path');
 
-const storage = multer.diskStorage({
-  destination: path.resolve(__dirname, '..', '..', 'files'),
-  filename: (_req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    const name = path.basename(file.originalname, ext);
+const upload = {
+  storage: multer.diskStorage({
+    destination: path.resolve(__dirname, '..', '..', 'files'),
+    filename: (req, file, callback) => {
+      const ext = path.extname(file.originalname);
+      const name = path.basename(file.originalname, ext);
 
-    cb(null, `${name.replace(/\s/g, '')}-${Date.now()}${ext}`);
-  },
-});
+      callback(null, `${name.replace(/\s/g, '')}-${Date.now()}${ext}`);
+    },
+  }),
+};
 
-module.exports = storage;
+module.exports = upload;
