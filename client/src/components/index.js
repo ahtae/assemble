c56608f53a5cf4644f9ef8581492008db6352fae
Kristy Li <kristyli20.0@gmail.com>
2020-11-16 19:52:53 -0500

    refactor: Export components into one file

---
+++ client/src/components/index.js
@@ -0,0 +1,11 @@
+import Home from './Home/Home';
+import Login from './Login/Login';
+import Register from './Register/Register';
+import EventForm from './EventForm/EventForm';
+import Events from './Events/Events';
+
+export { Home };
+export { Login };
+export { Register };
+export { EventForm };
+export { Events };
