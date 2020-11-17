703d262a197450c8a77a7c30a2f2ec726fff9a4e
Kristy Li <kristyli20.0@gmail.com>
2020-11-17 00:09:53 -0500

    refactor: Create a module for the HTTP requests associated with the auth routes

---
+++ client/src/services/auth.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+const baseUrl = '/api/auth/user/';
+
+const login = async (credentials) => {
+  const response = await axios.post(`${baseUrl}/login`, credentials);
+
+  return response.data;
+};
+
+const register = async (credentials) => {
+  const response = await axios.post(`${baseUrl}/register`, credentials);
+
+  return response.data;
+};
+
+const authService = { login, register };
+
+export default authService;
