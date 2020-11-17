78ac13e7e52382cb1ab44a8894e50649f7dab962
Kristy Li <kristyli20.0@gmail.com>
2020-11-17 15:28:37 -0500

    chore: Import and export NotFound component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,9 +3,11 @@ import Login from './Login/Login';
 import Register from './Register/Register';
 import EventForm from './EventForm/EventForm';
 import Events from './Events/Events';
+import NotFound from './NotFound/NotFound';
 
 export { Home };
 export { Login };
 export { Register };
 export { EventForm };
 export { Events };
+export { NotFound };
